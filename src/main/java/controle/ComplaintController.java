b5b2d34b0596ab6a8c05ba744057350096a211ed
Marlon Andrel <marlonmacf@gmail.com>
2016-06-27 19:59:57 -0300

    Helper for iago.

--- src/main/java/controle/ComplaintController.java
+++ src/main/java/controle/ComplaintController.java
@@ -14,18 +14,40 @@ public class ComplaintController {
     private Result result;
     
     public List<Complaint> index() {
+        // busca sapoha tudo.
+        
         List<Complaint> users = new ArrayList<>();
         return users;
     }
     
-    public List<Complaint> create() {
-        List<Complaint> users = new ArrayList<>();
-        return users;
+    public void create() {
+        // retorna somente a view.
     }
     
-    public List<Complaint> edit() {
-        List<Complaint> users = new ArrayList<>();
-        return users;
+    public Complaint store () {
+        // save.
+        
+        Complaint complaint = new Complaint();
+        return complaint;
+    }
+    
+    public Complaint edit(Integer idComplaint) {
+        // buscar o manolo pelo id pra editar.
+        
+        Complaint complaint = new Complaint();
+        return complaint;
+    }
+    
+    public Complaint update(Complaint complaint) {
+        // alera o cara aqui.
+        
+        return complaint;
+    }
+    
+    public Boolean destroy(Integer idComplaint) {
+        // kill him!!
+        
+        return true;// ou falso se der errado (try/catch)
     }
 
 }
