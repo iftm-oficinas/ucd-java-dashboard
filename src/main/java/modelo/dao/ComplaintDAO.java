ff01461f91ee71afd61a156dcba06b24a0315402
Marlon Andrel <marlonmacf@gmail.com>
2016-06-27 20:18:27 -0300

    DAOHelpers for iago.

--- src/main/java/modelo/dao/ComplaintDAO.java
+++ src/main/java/modelo/dao/ComplaintDAO.java
@@ -1,6 +1,9 @@
 package modelo.dao;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Complaint;
 
 public class ComplaintDAO {
 
@@ -8,5 +11,28 @@ public class ComplaintDAO {
 
     public ComplaintDAO(Connection connection) {
         this.connection = connection;
+    }
+    
+    public Complaint save(){
+        Complaint complaint = new Complaint();
+        return complaint;
+    }
+    
+    public Complaint update(Complaint complaint) {
+        return complaint;
+    }
+    
+    public Complaint fetchOne(Integer idComplaint) {
+        Complaint complaint = new Complaint();
+        return complaint;
+    }
+    
+    public List<Complaint> fetchAll() {
+        List<Complaint> complaints = new ArrayList<>();
+        return complaints;
+    }
+    
+    public Boolean delete(Integer idComplaint) {
+        return true;
     }
 }
--- src/main/java/modelo/dao/ComplaintPhotoDAO.java
+++ src/main/java/modelo/dao/ComplaintPhotoDAO.java
@@ -1,6 +1,9 @@
 package modelo.dao;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.ComplaintPhoto;
 
 public class ComplaintPhotoDAO {
 
@@ -8,5 +11,28 @@ public class ComplaintPhotoDAO {
 
     public ComplaintPhotoDAO(Connection connection) {
         this.connection = connection;
+    }
+
+    public ComplaintPhoto save() {
+        ComplaintPhoto complaintPhoto = new ComplaintPhoto();
+        return complaintPhoto;
+    }
+
+    public ComplaintPhoto update(ComplaintPhoto complaintPhoto) {
+        return complaintPhoto;
+    }
+
+    public ComplaintPhoto fetchOne(Integer idComplaintPhoto) {
+        ComplaintPhoto complaintPhoto = new ComplaintPhoto();
+        return complaintPhoto;
+    }
+
+    public List<ComplaintPhoto> fetchAll() {
+        List<ComplaintPhoto> complaintPhotos = new ArrayList<>();
+        return complaintPhotos;
+    }
+
+    public Boolean delete(Integer idComplaintPhoto) {
+        return true;
     }
 }
