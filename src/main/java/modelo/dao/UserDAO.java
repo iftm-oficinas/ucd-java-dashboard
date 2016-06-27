96f5b7ccf32b6aa9c83c337dc2f17991b613bfcb
Marlon Andrel <marlonmacf@gmail.com>
2016-06-27 20:10:52 -0300

    DAOUser helper for iago.

--- src/main/java/modelo/dao/UserDAO.java
+++ src/main/java/modelo/dao/UserDAO.java
@@ -1,6 +1,9 @@
 package modelo.dao;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.User;
 
 public class UserDAO {
 
@@ -8,5 +11,33 @@ public class UserDAO {
 
     public UserDAO(Connection connection) {
         this.connection = connection;
+    }
+    
+    public User save(){
+        User user = new User();
+        return user;
+    }
+    
+    public User update(User user) {
+        return user;
+    }
+    
+    public User fetchOne(Integer idUser) {
+        User user = new User();
+        return user;
+    }
+    
+    public List<User> fetchAll() {
+        List<User> users = new ArrayList<>();
+        return users;
+    }
+    
+    public Boolean delete(Integer idUser) {
+        return true;
+    }
+    
+    public List<User> fetchAllOrderByScore() {
+        List<User> users = new ArrayList<>();
+        return users;
     }
 }
