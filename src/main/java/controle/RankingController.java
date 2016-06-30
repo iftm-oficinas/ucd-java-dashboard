33804b57f1f671cd5fa4ff4323462523c04df5c0
Marlon Andrel <marlonmacf@gmail.com>
2016-06-29 21:43:43 -0300

    Reports.

--- src/main/java/controle/RankingController.java
+++ src/main/java/controle/RankingController.java
@@ -1,20 +1,34 @@
 package controle;
 
 import br.com.caelum.vraptor.Controller;
-import br.com.caelum.vraptor.Result;
-import java.util.ArrayList;
-import java.util.List;
-import javax.inject.Inject;
-import modelo.User;
+import br.com.caelum.vraptor.jasperreports.Report;
+import br.com.caelum.vraptor.jasperreports.ReportBuilder;
+import br.com.caelum.vraptor.jasperreports.download.ReportDownload;
+import static br.com.caelum.vraptor.jasperreports.formats.ExportFormats.pdf;
+import br.com.caelum.vraptor.observer.download.Download;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.dao.FactoryConnection;
 
 @Controller
 public class RankingController {
-
-    @Inject
-    private Result result;
     
-    public List<User> index() {
-        List<User> users = new ArrayList<>();
-        return users;
+    public void index() {
+        //Default index page.
+    }
+    
+    public Download report() {
+        Report report = null;
+        try {
+            report = new ReportBuilder()
+                    .withTemplate("ranking.jasper")
+                    .addParam("java.sql.Connection", FactoryConnection.getConnection())
+                    .withFileName("ranking")
+                    .build();
+        } catch (SQLException ex) {
+            Logger.getLogger(RankingController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return new ReportDownload(report, pdf());
     }
 }
